cf20f650202cb679fb506cff801c3093669f5288
FranRuizEsc <fran.ruiz.es@gmail.com>
2025-01-10 18:36:53 +0100

    Create main component and show it

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { ROUTES } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true })]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(ROUTES),]
 };
