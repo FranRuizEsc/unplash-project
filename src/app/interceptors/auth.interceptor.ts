95357eff41a5b22c7924270f9027bf19757ee0b6
FranRuizEsc <60076220+FranRuizEsc@users.noreply.github.com>
2025-01-20 20:20:47 +0100

    add interceptor

---
+++ src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,14 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+
+    const token = environment.apiKey
+
+    if (token) {
+        req = req.clone({
+            headers: req.headers.set('Authorization', `Client-ID ${token}`)
+        })
+    }
+
+    return next(req);
+}
