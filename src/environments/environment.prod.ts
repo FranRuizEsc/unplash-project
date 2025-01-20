290005cdd88c9b0b7600c2fedf971e81fadec2e4
FranRuizEsc <60076220+FranRuizEsc@users.noreply.github.com>
2025-01-20 20:10:34 +0100

    add environment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+	production: false,
+	apiKey: 'CECRET_API_KEY',
+}; 
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+	production: false,
+	apiUrl: 'https://api.unsplash.com',
+	apiKey: 'SECRET_API_KEY'
+}; 
