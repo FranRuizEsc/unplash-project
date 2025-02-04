5c017de584380c689d04589b13adff1bc93f1192
FranRuizEsc <60076220+FranRuizEsc@users.noreply.github.com>
2025-02-04 17:50:12 +0100

    feat(UP-18): Create photo deatil component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,5 +8,9 @@ export const ROUTES: Route[] = [
 	{
 		path: 'search',
 		loadComponent: () => import('./components/search-result/search-result.component').then((m) => m.SearchResultComponent),
+	},
+	{
+		path: 'photo:/photo_id',
+		loadComponent: () => import('./components/photo-detail/photo-detail.component').then((m) => m.PhotoDetailComponent)
 	}
 ]
